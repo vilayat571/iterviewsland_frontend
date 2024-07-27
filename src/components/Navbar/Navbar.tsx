c815dd741ef10d5ce36540e12706cff9ccf500a5
Vilayat Safarov <vilayat571@gmail.com>
2024-07-27 11:23:22 -0700

    little configuration on navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,7 +10,8 @@ const Navbar = () => {
     py-3 xl:my-8 lg:my-8 md:my-4 sm:my-4 rounded-full text-[#fff] xl:px-12 lg:px-12 md:px-3 sm:px-2 flex justify-between items-center  "
     >
       <Logo />
-      <button className="cursor-pointer">
+      <button className="cursor-pointer
+      xl:hidden sm:block md:block lg:hidden">
       <FontAwesomeIcon icon={faBars}
       className="text-xl text-[#fff] px-2 rounded-[3px] py-[6px] bg-[#ff790bee] relative right-2"
       />
@@ -18,7 +19,7 @@ const Navbar = () => {
       </button>
       <div className=" tracking-widest text-[#f1efef]
       xl:flex lg:flex md:hidden sm:hidden
-      items-center gap-5 text-base relative r-4">
+      items-center gap-5 text-base relative right-3">
         <Link
           onClick={() => alert("Bu hissə tam hazır deyil. Üzür istəyirik!")}
           to="/"
