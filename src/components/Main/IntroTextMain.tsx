bf2686037e1ab24e1cfe65177c1d3b905d15d75a
Vilayat Safarov <vilayat571@gmail.com>
2024-07-27 18:14:29 -0700

    some changes in home page

---
+++ src/components/Main/IntroTextMain.tsx
@@ -0,0 +1,33 @@
+
+const IntroTextMain = () => {
+  return (
+    <div className="mt-8 mb-24 px-4">
+    <p
+      className="text-[#f1f1f1] 
+xl:text-center lg:text-center md:text-left sm:text-left 
+xl:text-[45px] lg:text-[45px] md:text-[35px] sm:text-3xl  font-semibold"
+    >
+      Durmadan inkişaf et!
+    </p>
+    <p
+      className="text-white 
+leading-[28px]
+xl:text-center lg:text-center md:text-left sm:text-left 
+text-base tracking-widest
+xl:mt-3 lg:mt-3 md:mt-2 sm:mt-3 "
+    >
+      <u className="text-orange-400 text-xl font-semibold">interviewsland</u> ilə
+      biliklərini gücləndir və iş həyatında daha uğurlu ol!
+      <br className="sm:hidden xl:block md:hidden lg:block" />
+      Yiyələndiyiniz informasiya texnalogiyaları ilə əlaqəli sahədən aslı
+      olmayaraq burada
+      <br className="sm:hidden xl:block md:hidden lg:block" />
+      siz biliklərinizi daha da artırmaq üçün Azərbaycan dilində resursdan
+      istifadə edə bilərsiniz.
+      <br className="sm:hidden xl:block md:hidden lg:block" />
+    </p>
+  </div>
+  )
+}
+
+export default IntroTextMain
--- src/pages/Main/App.tsx
+++ src/pages/Main/App.tsx
@@ -1,3 +1,4 @@
+import IntroTextMain from "../../components/Main/IntroTextMain";
 import Layout from "../../Layout/Layout";
 
 export interface ICategory {
@@ -7,14 +8,28 @@ export interface ICategory {
 const App = () => {
   return (
     <Layout>
-      <div className="h-screen
+      <div
+        className="h-screen items-center flex flex-col
       xl:w-4/5 lg:w-4/5 md:w-full sm:w-full
       text-white text-3xl 
-      ">
+      "
+      >
+        <img
+        className="xl:w-[270px] lg:w-[270px] md:w-[220px] sm:w-[200px] mt-6 object-cover"
+          src="https://r2.erweima.ai/imgcompressed/compressed_57b6009281b58ecf2785fefb6ad493a3.webp"
+          alt=""
+        />
+        <IntroTextMain />
 
-
-
-
+        <div>
+          <p
+            className="text-[#f1f1f1] 
+text-left
+text-3xl  font-semibold"
+          >
+            Xidmətlər
+          </p>
+        </div>
       </div>
     </Layout>
   );
