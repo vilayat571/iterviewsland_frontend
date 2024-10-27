8ba6552b0c60844de745c33f37d586de407e3bf9
vilayat571 <vilayat571@gmail.com>
2024-10-27 16:06:15 +0400

    debuginh for the first production testing

--- src/components/Share/ExampleText.tsx
+++ src/components/Share/ExampleText.tsx
@@ -1,4 +1,4 @@
-import { faArrowLeft, faShare, faShareNodes, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
